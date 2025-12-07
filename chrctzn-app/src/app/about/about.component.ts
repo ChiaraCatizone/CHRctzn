a5983aff7664c899dfe232aef6521e8c24a0d4bb
ChiaraCati <chiara.catizone@studio.unibo.it>
2025-12-07 13:20:50 +0100

    up de

---
+++ chrctzn-app/src/app/about/about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-about',
+  template: `<h1>About</h1><p>We build neat stuff.</p>`
+
+})
+export class AboutComponent {
+
+}
